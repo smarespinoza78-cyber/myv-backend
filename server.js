772b2bf046a47d1bd4186e46cd34e8787539ec41
smarespinoza78-cyber <smarespinoza78@gmail.com>
2026-03-05 08:17:40 -0600

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 app.use(express.static('public'));
 
